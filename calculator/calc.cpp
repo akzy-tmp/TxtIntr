8c90cedaccf29b03eba237a468c28d39ed536b97
akzy-tmp <kzsilly1@gmail.com>
2024-11-24 07:13:33 +0300

    calculator

--- calculator/calc.cpp
+++ calculator/calc.cpp
@@ -1,59 +1,81 @@
 #include <iostream>
 #include <getopt.h>
-#include <unistd.h>
 #include <cmath>
 #include <vector>
-#include <typeinfo>
 #include <string>
-#include <string.h>
 #include <algorithm>
 
 using namespace std;
 
-int main(int argc, char **argv)
-{
-    if (argc==1) {
-        cout << "Parametrs:\n a - среднее арифметическое\n m  - медиана\n у нас 9-11 операнд\n";
-        exit(1);
+void printHelp() {
+    cout << "Usage:\n";
+    cout << "  -o, --operation <operation>  Specify the operation to perform (average or median)\n";
+    cout << "  Operands must be provided as positional parameters (e.g., 45 13 -2).\n";
+    cout << "  You must provide between 7 and 10 operands.\n";
+}
+
+float calculateAverage(const vector<float>& operands) {
+    float sum = 0;
+    for (float num : operands) {
+        sum += num;
+    }
+    return sum / operands.size();
+}
+
+float calculateMedian(vector<float>& operands) {
+    sort(operands.begin(), operands.end());
+    int n = operands.size();
+    
+    if (n % 2 == 0) {
+        return (operands[n / 2 - 1] + operands[n / 2]) / 2;
+    } else {
+        return operands[n / 2];
+    }
+}
+
+int main(int argc, char **argv) {
+    // Проверка на количество аргументов
+    if (argc < 9 || argc > 12) { // от 7 до 10 операндов + опция и операция
+        printHelp();
+        return 1;
     }
 
-    int opt, i, d, st=0, k=1;
-    float z, a=0, b, res;
-    for (i=2; i<argc; i++){
-        st+=1;
-    }
-    if ((st < 9) or (st > 11)){
-        return 0;
-    }
-    while ((opt = getopt (argc, argv, "a:m:")) != -1) {
+    string operation;
+    vector<float> operands;
+
+    // Обработка параметров командной строки
+    int opt;
+    while ((opt = getopt(argc, argv, "o:")) != -1) {
         switch (opt) {
-        case 'a':
-            for(i=3; i<argc; i++) {
-                b = strtol(argv[i], NULL, 10);
-                a = a + b;
-                k+=1;
-            }
-            z = a / k;
-            cout<< "RESULT: "<< z << endl;
-            break;
-        case 'm':
-            vector< int > arr(argc-2);
-            for(i=3; i<argc-2; i++) {
-                b = strtol(argv[i+2], NULL, 10);
-                arr[i] = b;
-            }
-            sort(arr.begin(), arr.end());
-            if (argc-2 == 9 or argc-2 == 11) {
-                int k = ceil((argc-2) / 2);
-                res = arr[k];
-            }
-            else if (argc-2 == 10) {
-                int k = ((argc-2) / 2);
-                z = arr[k-1]+arr[k];
-                res = z / 2;
-            }
-            cout<< "RESULT: "<< res << endl;
-            break;
+            case 'o':
+                operation = optarg; // Сохраняем операцию
+                break;
+            default:
+                printHelp();
+                return 1;
         }
     }
+
+    // Сохраняем операнды
+    for (int i = optind; i < argc; i++) {
+        operands.push_back(strtof(argv[i], nullptr));
+    }
+
+    // Проверка на количество операндов
+    if (operands.size() < 7 || operands.size() > 10) {
+        printHelp();
+        return 1;
+    }
+
+    // Выполнение операции
+    if (operation == "average") {
+        cout << "RESULT: " << calculateAverage(operands) << endl;
+    } else if (operation == "median") {
+        cout << "RESULT: " << calculateMedian(operands) << endl;
+    } else {
+        printHelp();
+        return 1;
+    }
+
+    return 0;
 }
